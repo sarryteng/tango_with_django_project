13d99c773966d75fa51ec6391f8c10062194a270
“Sarry <2721338t@student.gla.ac.uk>
2023-01-29 00:56:49 +0000

    end of chapter 5

--- populate_rango.py
+++ populate_rango.py
@@ -23,9 +23,9 @@ def populate():
     other_pages = [{'title':'Bottle','url':'http://bottlepy.org/docs/dev/'},
                    {'title':'Flask','url':'http://flask.pocoo.org'} ]
     
-    cats = {'Python': {'pages': python_pages}, 
-            'Django': {'pages': django_pages}, 
-            'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64}, 
+            'Django': {'pages': django_pages, 'views': 64, 'likes': 32}, 
+            'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
 
     # If you want to add more categories or pages,
     # add them to the dictionaries above.
@@ -33,7 +33,7 @@ def populate():
     # The code below goes through the cats dictionary, then adds each category,
     # and then adds all the associated pages for that category.
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
 
@@ -49,8 +49,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.views=views
+    c.likes=likes
     c.save()
     return c
 
--- rango/admin.py
+++ rango/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
 from rango.models import Category, Page
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title','category', 'url')
+
+# Register your models here.
 admin.site.register(Category)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
